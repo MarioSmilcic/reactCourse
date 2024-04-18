061962b02cbdb2bf1f30ed19b533d4697b691e61
Mario <mariosmilcic@gmail.com>
2024-04-18 13:02:43 +0200

    Added Order component, and split code in ItemsInCart

--- src/features/Items/components/ItemsInCart.jsx
+++ src/features/Items/components/ItemsInCart.jsx
@@ -1,71 +1,92 @@
 import { useCartStore } from "../../../stores/cart/cart";
-import { useClicksStore } from "../../../stores/clicks/clicks.store";
-import "../items.style.css";
-import { useNavigate } from "react-router-dom";
+import Orders from "./Orders";
 
 const ItemsInCart = () => {
-  const { cart, resetCart, removeItemFromCart } = useCartStore();
-  const { decrementNumberOfClicks, resetClicksCount } = useClicksStore();
-
-  const navigate = useNavigate();
-  const handleOrderClick = () => {
-    navigate("/items");
-  };
+  const { cart } = useCartStore();
 
   const emptyCart = "No items in cart";
 
-  const totalSum = cart.reduce((total, item) => {
-    return total + item.price;
-  }, 0);
-
-  const items = (
-    <div className="items_in_cart">
-      {cart.map((item) => (
-        <div key={Math.random()} className="item_in_cart">
-          <img src={item.image} alt={item.title} className="articleImg" />
-          <h3>{item.title}</h3>
-          <p>${item.price}</p>
-          <img
-            className="recycle"
-            src="https://cdn-icons-png.flaticon.com/128/11041/11041893.png"
-            alt="recycle"
-            onClick={() =>
-              removeItemFromCart(item.id) & decrementNumberOfClicks()
-            }
-          />
-        </div>
-      ))}
-      <div>
-        <h3>Total price for all items: {totalSum.toFixed(2)}</h3>
-        <div className="buttons">
-          <button
-            className="order_button"
-            onClick={() =>
-              resetCart() & resetClicksCount() & handleOrderClick()
-            }
-          >
-            Order
-          </button>
-          <button
-            className="clear_cart"
-            onClick={() => {
-              resetCart() & resetClicksCount();
-            }}
-          >
-            Clear Cart
-          </button>
-        </div>
-      </div>
-    </div>
-  );
   return (
     <div>
-      <div>{cart.length === 0 ? emptyCart : items}</div>
+      <div>{cart.length === 0 ? emptyCart : <Orders />}</div>
     </div>
   );
 };
 
 export default ItemsInCart;
+
+///////////////OLDER SOLUTION////////////////////////////////////
+
+// import { useCartStore } from "../../../stores/cart/cart";
+// import { useClicksStore } from "../../../stores/clicks/clicks.store";
+// import "../items.style.css";
+// import { useNavigate } from "react-router-dom";
+
+// const ItemsInCart = () => {
+//   const { cart, resetCart, removeItemFromCart } = useCartStore();
+//   const { decrementNumberOfClicks, resetClicksCount } = useClicksStore();
+
+//   const navigate = useNavigate();
+//   const handleOrderClick = () => {
+//     navigate("/items");
+//   };
+
+//   const emptyCart = "No items in cart";
+
+//   const totalSum = cart.reduce((total, item) => {
+//     return total + item.price;
+//   }, 0);
+
+//   const items = (
+//     <div className="items_in_cart">
+//       {cart.map((item) => (
+//         <div key={Math.random()} className="item_in_cart">
+//           <img src={item.image} alt={item.title} className="articleImg" />
+//           <h3>{item.title}</h3>
+//           <p>${item.price}</p>
+//           <img
+//             className="recycle"
+//             src="https://cdn-icons-png.flaticon.com/128/11041/11041893.png"
+//             alt="recycle"
+//             onClick={() =>
+//               removeItemFromCart(item.id) & decrementNumberOfClicks()
+//             }
+//           />
+//         </div>
+//       ))}
+//       <div>
+//         <h3>Total price for all items: {totalSum.toFixed(2)}</h3>
+//         <div className="buttons">
+//           <button
+//             className="order_button"
+//             onClick={() =>
+//               resetCart() & resetClicksCount() & handleOrderClick()
+//             }
+//           >
+//             Order
+//           </button>
+//           <button
+//             className="clear_cart"
+//             onClick={() => {
+//               resetCart() & resetClicksCount();
+//             }}
+//           >
+//             Clear Cart
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+//   return (
+//     <div>
+//       <div>{cart.length === 0 ? emptyCart : items}</div>
+//     </div>
+//   );
+// };
+
+// export default ItemsInCart;
+
+///////////////////////FIRST SOLUTION///////////////////////////////////////////////
 
 {
   /* {cart.length === 0
